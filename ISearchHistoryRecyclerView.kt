5d8e8a1a021163adffed72d0b2d029c3495a0612
sangwoong62 <sangwoong62@gmail.com>
2021-05-05 13:57:14 +0900

    리사이클러뷰에 아이템 삭제기능 코드 참고용인데 혹시몰라서 첨부함.

---
+++ ISearchHistoryRecyclerView.kt
@@ -0,0 +1,11 @@
+package com.pill.what
+
+interface ISearchHistoryRecyclerView {
+
+    // 검색 아이템 삭제 버튼 클릭
+    fun onSearchItemDeleteClicked(position: Int)
+
+    // 검색 버튼 클릭
+    fun onSearchItemClicked(position: Int)
+
+}
