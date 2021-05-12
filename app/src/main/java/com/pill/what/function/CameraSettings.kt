9be7fa59fb4f3e3f86c7f550db80e1e8750383e2
tkarlz <tjdudrb97@gmail.com>
2021-05-12 17:30:10 +0900

    Save ImageFile from camera or gallery

--- app/src/main/java/com/pill/what/function/CameraSettings.kt
+++ app/src/main/java/com/pill/what/function/CameraSettings.kt
@@ -3,6 +3,7 @@ package com.pill.what.function
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
@@ -34,27 +35,15 @@ class CameraSettings(val activity: MainActivity) {
         cameraStartForResult = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
 //            val intent = result.data
-                val bitmap: Bitmap
-                val file = File(curPhotoPath)
-                bitmap = if (Build.VERSION.SDK_INT < 28){
-                    MediaStore.Images.Media.getBitmap(activity.contentResolver, Uri.fromFile(file))
-                    //                iv_profile.setImageBitmap(bitmap)
-                } else {
-                    val decode = ImageDecoder.createSource(
-                        activity.contentResolver,
-                        Uri.fromFile(file)
-                    )
-                    ImageDecoder.decodeBitmap(decode)
-                    //                iv_profile.setImageBitmap(bitmap)
-                }
-                savePhoto(bitmap)
+                // TODO server communication
             }
         }
 
         galleryStartForResult = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
-//            val intent = result.data
-                Log.e("Asdf", "image")
+                val intent = result.data
+                saveImageFile(intent?.data!!)
+                // TODO server communication
             }
         }
 
@@ -108,7 +97,7 @@ class CameraSettings(val activity: MainActivity) {
             .setPermissionListener(permission)
             .setRationaleMessage("카메라를 사용하시려면 권한을 허용해주세요.")
             .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] -> [권한] 항목에서 허용해주세요.")
-            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
+            .setPermissions(android.Manifest.permission.CAMERA)
             .check()
     }
 
@@ -126,27 +115,26 @@ class CameraSettings(val activity: MainActivity) {
             .setPermissionListener(permission)
             .setRationaleMessage("갤러리에 접근하시려면 권한을 허용해주세요.")
             .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] -> [권한] 항목에서 허용해주세요.")
-            .setPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .setPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             .check()
     }
 
     private fun createImageFile(): File? {
+        val flist = activity.filesDir.listFiles { _, name ->
+            name.endsWith(".jpg")
+        }
+        flist.forEach {
+            it.delete()
+        }
         val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.KOREA).format(Date())
-        val storageDir: File? = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val storageDir: File? = activity.filesDir
         return File.createTempFile("JPEG_${timestamp}_",".jpg",storageDir)
             .apply { curPhotoPath = absolutePath }
     }
 
-    private fun savePhoto(bitmap: Bitmap){
-        val folderPath = Environment.getExternalStorageDirectory().absolutePath + "/Pictures/"
-        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.KOREA).format(Date())
-        val fileName = "${timestamp}.jpeg"
-        val folder = File(folderPath)
-        if(!folder.isDirectory){
-            folder.mkdirs()
-        }
-        val out = FileOutputStream(folderPath + fileName)
+    private fun saveImageFile(uri: Uri) {
+        val bitmap = BitmapFactory.decodeStream(activity.contentResolver.openInputStream(uri))
+        val out = FileOutputStream(curPhotoPath)
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-        Toast.makeText(activity,"사진이 앨범에 저장되었습니다.", Toast.LENGTH_SHORT).show()
     }
 }
